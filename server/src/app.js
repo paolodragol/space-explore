32a75a94e5458cf13382ce01b9b5acedea6181d4
Paolo Drago Leon s296101 <100195874+paolodragol@users.noreply.github.com>
2025-03-17 19:41:31 +0100

    remove node_modules

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
 // use built-in express json middleware
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 app.use(express.json());
 app.use(planetsRouter);
 
