13562396eb8269cc7e5cf00a5f18d12d7b180ef6
Paolo Drago Leon s296101 <100195874+paolodragol@users.noreply.github.com>
2025-03-22 17:38:37 +0100

    feat: add loading of plantes on server startup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,45 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
+
+const loadPlanetsData = async () => {
+  console.log('Start parsing file...');
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+    )
+      .pipe(
+        parse({
+          comment: '#',
+          columns: true,
+        })
+      )
+      .on('data', (data) => {
+        if (isHabitable(data)) habitablePlanets.push(data);
+      })
+      .on('error', (error) => {
+        console.log(error);
+        reject(error);
+      })
+      .on('end', () => {
+        console.log(`${habitablePlanets.length} habitable plantes found`);
+        resolve();
+      });
+  });
+};
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
