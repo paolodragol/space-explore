84beebbe41c8fb646a5408d1f6b7e97c621114dc
Paolo Drago Leon s296101 <100195874+paolodragol@users.noreply.github.com>
2026-04-04 19:25:31 +0200

    feat: add MongoDB connection to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,26 @@
 const http = require('http');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
+// emit events when the connection is ready
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 const startServer = async () => {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
